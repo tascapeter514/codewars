c98f53bec489e48bbad4fcd2d5e4f73ecee3da10
petertasca <tascapeter@gmail.com>
2025-01-22 09:27:24 -0500

    building block javascript solution

---
+++ javascript/7kyu/buildingBlocks.js
@@ -0,0 +1,93 @@
+// Write a class Block that creates a block (Duh..)
+// Requirements
+
+// The constructor should take an array as an argument, this will contain 3 integers of the form [width, length, height] from which the Block should be created.
+
+// Define these methods:
+
+// `getWidth()` return the width of the `Block`
+
+// `getLength()` return the length of the `Block`
+
+// `getHeight()` return the height of the `Block`
+
+// `getVolume()` return the volume of the `Block`
+
+// `getSurfaceArea()` return the surface area of the `Block`
+
+// Examples
+
+//     let b = new Block([2,4,6]) -> creates a `Block` object with a width of `2` a length of `4` and a height of `6`
+//     b.getWidth() // -> 2
+    
+//     b.getLength() // -> 4
+    
+//     b.getHeight() // -> 6
+    
+//     b.getVolume() // -> 48
+    
+//     b.getSurfaceArea() // -> 88
+
+// Note: no error checking is needed
+
+// Any feedback would be much appreciated
+
+
+// MY SOLUTION
+
+class Block{
+
+    constructor(data){
+        console.log(data)
+        const [width, length, height] = data;
+
+        this.width = width;
+        this.length = length;
+        this.height = height;
+    }
+
+    getWidth() {
+        return this.width
+    }
+
+    getLength() {
+        return this.length
+    }
+
+    getHeight() {
+        return this.height
+    }
+
+    getVolume() {
+        return this.height * this.length * this.width
+    }
+
+    getSurfaceArea() {
+        const surfaceArea = 2 * (this.length * this.width + this.length * this.height + this.height * this.width)
+        return surfaceArea
+    }
+
+
+
+  }
+
+// CODEWARS SOLUTION
+
+// class Block {
+//     constructor(data){
+//       [this.w, this.l, this.h] = data;
+//     }
+//     getWidth(){ return this.w }
+//     getLength(){ return this.l }
+//     getHeight(){ return this.h }
+//     getVolume(){
+//       let {l, w, h} = this;
+//       return w*l*h;
+//     }
+//     getSurfaceArea(){
+//       let {l, w, h} = this;
+//       return 2 * (l*w + l*h + w*h);
+//     }
+//   }
+
+
